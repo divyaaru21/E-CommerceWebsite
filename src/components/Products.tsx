f994c0205e85ef8428f25c3c11b045e6ac8d9ed5
divyaaru21 <126807372+divyaaru21@users.noreply.github.com>
2023-05-11 16:41:16 +0530

    Add files via upload

---
+++ src/components/Products.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import styles from '../App.module.css';
+
+import { ShoesDetails } from '../static/shoedata';
+import Item from './Item';
+
+const ProductItems = () => {
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading1}>Products</h1>
+      <div className={styles.grid}>
+        {ShoesDetails.map((shoe, idx) => <Item shoe={shoe} idx={idx} />)}
+      </div>
+    </div>
+  );
+}
+
+export default ProductItems;
